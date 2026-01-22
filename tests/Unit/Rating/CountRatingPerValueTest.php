b9a3a4e4c1658185921155fedd6ca7b52014a22b
Quentin Macé <quentin.mace@quarksgroup.com>
2026-01-22 15:26:09 +0100

    Fix incorrect rating counts in CountRatingPerValueTest test data

--- tests/Unit/Rating/CountRatingPerValueTest.php
+++ tests/Unit/Rating/CountRatingPerValueTest.php
@@ -43,8 +43,8 @@ class CountRatingPerValueTest extends TestCase
                 [0,4,0,0,0]
             ],
             [
-                [1,2,3,5,5],
-                [1,1,1,0,2]
+                [1,2,3,4,5],
+                [1,1,1,1,1]
             ],
             [
                 [],
