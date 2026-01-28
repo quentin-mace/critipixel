11dca99784717314003fa6ae669b6e8a0b6f5543
Quentin Macé <quentin.mace@quarksgroup.com>
2026-01-28 16:08:46 +0100

    WIP: Add filter test for video games by tags

--- tests/Functional/VideoGame/FilterTest.php
+++ tests/Functional/VideoGame/FilterTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\VideoGame;
 
+use App\Model\Entity\Tag;
+use App\Model\Entity\VideoGame;
 use App\Tests\Functional\FunctionalTestCase;
 
 final class FilterTest extends FunctionalTestCase
@@ -25,5 +27,27 @@ final class FilterTest extends FunctionalTestCase
         $this->client->submitForm('Filtrer', ['filter[search]' => 'Jeu vidéo 49'], 'GET');
         self::assertResponseIsSuccessful();
         self::assertSelectorCount(1, 'article.game-card');
+    }
+
+    public function testShouldFilterVideoGamesByTags(): void
+    {
+        $data = [0, 2];
+        // Récupérer la liste des tags depuis la BDD
+        $entityManager = $this->getEntityManager();
+        $tags = $entityManager->getRepository(Tag::class)->findAll();
+        $tagsIds = array_map(fn (int $value) => $tags[$value]->getId(), $data);
+
+        $videoGames = $entityManager->getRepository(VideoGame::class)->findBy(['tags' => $tagsIds]);
+        dd($videoGames);
+
+        // se connecter à la liste des jeux
+        $this->get('/');
+
+        // Soumettre le formulaire de filtre avec les données fournies
+        $this->client->submitForm('Filtrer', ['filter[tags]' => $tagsIds], 'GET');
+
+        // Vérifier que le nombre de résultats corresponds à ce qui est présent en BDD
+
+
     }
 }
