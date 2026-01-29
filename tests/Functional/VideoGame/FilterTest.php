f1a0efa7c2fd209ccdc24bc4edd7db93705260ed
Quentin Macé <quentin.mace@quarksgroup.com>
2026-01-29 16:25:02 +0100

    Add method to filter video games by tags and enhance related functional test

--- tests/Functional/VideoGame/FilterTest.php
+++ tests/Functional/VideoGame/FilterTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\VideoGame;
 
+use App\Doctrine\Repository\VideoGameRepository;
 use App\Model\Entity\Tag;
 use App\Model\Entity\VideoGame;
 use App\Tests\Functional\FunctionalTestCase;
@@ -29,25 +30,49 @@ final class FilterTest extends FunctionalTestCase
         self::assertSelectorCount(1, 'article.game-card');
     }
 
-    public function testShouldFilterVideoGamesByTags(): void
+    /**
+     * @dataProvider provideTagsData
+     */
+    public function testShouldFilterVideoGamesByTags(array $data = []): void
     {
-        $data = [0, 2];
         // Récupérer la liste des tags depuis la BDD
         $entityManager = $this->getEntityManager();
-        $tags = $entityManager->getRepository(Tag::class)->findAll();
-        $tagsIds = array_map(fn (int $value) => $tags[$value]->getId(), $data);
 
-        $videoGames = $entityManager->getRepository(VideoGame::class)->findBy(['tags' => $tagsIds]);
-        dd($videoGames);
+        $tags = $entityManager->getRepository(Tag::class)->findAll();
+        $selectedTagIds = array_map(fn (int $value) => $tags[$value]->getId(), $data);
+
+        $videoGameRepository = $this->service(VideoGameRepository::class);
+        $videoGames = $videoGameRepository->getVideoGamesByTagIds($selectedTagIds);
 
         // se connecter à la liste des jeux
-        $this->get('/');
+        $crawler = $this->get('/');
 
         // Soumettre le formulaire de filtre avec les données fournies
-        $this->client->submitForm('Filtrer', ['filter[tags]' => $tagsIds], 'GET');
+        $form = $crawler->selectButton('Filtrer')->form();
+        $select = $form['filter[tags]'];
+        foreach ($data as $value){
+            $checkbox = $select[$value];
+            $checkbox->tick();
+        }
+        $this->client->submit($form);
 
-        // Vérifier que le nombre de résultats corresponds à ce qui est présent en BDD
+        // Vérifier que le nombre de résultats correspond à ce qui est présent en BDD
+        $shouldFilter = $data !== [];
+        $expectedResults = $data !== [] ? count($videoGames) : 10;
+        self::assertResponseIsSuccessful();
+        self::assertSelectorCount($expectedResults, 'article.game-card');
 
+        // ToDo : Trouver comment gerer le submit d'un tag inexistant
 
+    }
+
+    public function provideTagsData(): iterable
+    {
+        return [
+            [[0, 1]],
+            [[2]],
+            [[0, 2, 3]],
+            [[]],
+        ];
     }
 }
